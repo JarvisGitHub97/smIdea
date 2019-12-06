fb7fd1f98cfad75c97dbd3b7828ea1968cc67005
Jarvis <598494198@qq.com>
2019-12-06 20:11:10 +0800

    webpack

---
+++ todo/todoVue/src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo">
+    {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'hellov'
+    }
+  }
+}
+</script>
+
+<style>
+.todo {
+  color: #ccc;
+}
+</style>
