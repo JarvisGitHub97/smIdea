9e07a5753f9e486ec64d02db01e0e11f2e88a9eb
Jarvis <598494198@qq.com>
2019-12-10 23:56:33 +0800

    finish demand

--- todo/todoVue/src/app.vue
+++ todo/todoVue/src/app.vue
@@ -3,17 +3,20 @@
   <div class="gray">
   </div>
   <Header></Header>
+  <Todo></Todo>
   <Footer></Footer>
 </div>
 </template>
 
 <script>
 import Header from './todo/header.vue';
-import Footer from './todo/footer.jsx'
+import Footer from './todo/footer.jsx';
+import Todo from "./todo/todo.vue";
 export default {
   components: {
     Header,
-    Footer
+    Footer,
+    Todo
   }
 }
 </script>
